fa9e5d7b39520e8d94ef1840de237a8349197ba4
Filipe Silva <silvam.filipe@gmail.com>
2013-10-20 22:29:02 +0000

    Initializing base class.

---
+++ src/Slick/Common/Exception/BadConstructorException.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Bad Constructor Exception
+ * 
+ * @package   Slick\Common\Exception
+ * @author    Filipe Silva <silvam.filipe@gmail.com>
+ * @copyright 2014 Filipe Silva
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ * @since     Version 1.0.0
+ */
+
+namespace Slick\Common\Exception;
+
+/**
+ * BadConstructorException
+ *
+ * @package   Slick\Common\Exception
+ * @author    Filipe Silva <silvam.filipe@gmail.com>
+ */
+class BadConstructorException extends \LogicException
+    implements ExceptionInterface
+{
+    
+}
+
