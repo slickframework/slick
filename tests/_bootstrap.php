eacf273530ed35fd2d13ef2e75cafe62036440c0
Filipe Silva <filipe.silva@sata.pt>
2013-11-04 15:59:09 -0100

    Fixing bootstrap for codeception in PHPCI

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -3,7 +3,7 @@
 // This is global bootstrap for autoloading
 
 $projectRoot = dirname(dirname(__FILE__));
-$loader = $projectRoot . '/vendor/autoload.php';
+$loader = include($projectRoot . '/vendor/autoload.php');
 
 if (!class_exists('Slick\Common\Base')) {
     $loader->add('Slick', $projectRoot . '/src');
