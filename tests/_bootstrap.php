64156ee79d560873812cc92306c1c5c1da9d29a0
Filipe Silva <silvam.filipe@gmail.com>
2013-11-04 02:01:19 -0100

    Fixing autoload for codecption tests.

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,2 +1,11 @@
 <?php
-// This is global bootstrap for autoloading 
+
+// This is global bootstrap for autoloading
+
+$projectRoot = dirname(dirname(dirname(__FILE__)));
+$loader = $projectRoot . '/vendor/autoload.php';
+
+if (!class_exists('Slick\Common\Base')) {
+    $loader->add('Slick', $projectRoot . '/src');
+    $loader->register();
+} 
