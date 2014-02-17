25e58cd710385d1131c6f087e910e641cf0391fb
Filipe Silva <filipe.silva@sata.pt>
2014-02-17 19:01:08 -0100

    Finishing MVC

---
+++ src/Slick/Mvc/Model.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fsilva
+ * Date: 2/17/14
+ * Time: 5:32 PM
+ */
+
+namespace Slick\Mvc;
+
+
+class Model {
+
+} 
---
+++ tests/unit/Mvc/ApplicationTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace Mvc;
+use Codeception\Util\Stub;
+
+class ApplicationTest extends \Codeception\TestCase\Test
+{
+   /**
+    * @var \CodeGuy
+    */
+    protected $codeGuy;
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testMe()
+    {
+
+    }
+
+}
