e5215e828944bdefd9dba215ffee1d036c65e98f
Filipe Silva <filipe.silva@sata.pt>
2014-02-18 17:36:37 -0100

    Adding version information to the framework

---
+++ src/Slick/Version/Version.php
@@ -0,0 +1,91 @@
+<?php
+
+/**
+ * Version
+ *
+ * @package    Slick\Version
+ * @author     Filipe Silva <silvam.filipe@gmail.com>
+ * @copyright  2014 Filipe Silva
+ * @license    http://www.opensource.org/licenses/mit-license.php MIT License
+ * @since      Version 1.0.0
+ */
+
+namespace Slick\Version;
+
+/**
+ * Version
+ *
+ * @package    Slick\Version
+ * @author     Filipe Silva <silvam.filipe@gmail.com>
+ */
+final class Version
+{
+    /**
+     * Slick Framework version identification
+     * @see Version::compare
+     */
+    const VERSION = '1.0.0dev';
+
+    /**
+     * The latest stable version available
+     *
+     * @var string
+     */
+    protected static $_latestVersion;
+
+    /**
+     * Compare the provided version with current Slick version
+     *
+     * @param $version
+     *
+     * @return int -1 if the $version is older,
+     *      0 if they are the same,
+     *      and +1 if $version is newer.
+     */
+    public static function compare($version)
+    {
+        $version = strtolower($version);
+        $version = preg_replace('/(\d)pr(\d?)/', '$1a$2', $version);
+        return version_compare($version, strtolower(static::VERSION));
+    }
+
+    /**
+     * Fetches the version of the latest stable release.
+     *
+     * @return string
+     */
+    public static function getLatest()
+    {
+        if (is_null(static::$_latestVersion)) {
+            static::$_latestVersion = 'not available';
+            $url = "https://api.github.com/repos/slickframework/slick/git/refs/tags/v";
+            $data = @file_get_contents($url);
+            if ($data) {
+                $apiResponse = json_decode($data, true);
+
+                // Simplify the API response into a simple array of version numbers
+                $tags = array_map(function ($tag) {
+                    return substr($tag['ref'], 10); // Reliable because we're filtering on 'refs/tags/release-'
+                }, $apiResponse);
+
+                // Fetch the latest version number from the array
+                static::$_latestVersion = array_reduce($tags, function ($a, $b) {
+                    return version_compare($a, $b, '>') ? $a : $b;
+                });
+            }
+        }
+        return static::$_latestVersion;
+    }
+
+    /**
+     * Returns true if the running version of Zend Framework is
+     * the latest (or newer??) than the latest tag on GitHub,
+     * which is returned by static::getLatest().
+     *
+     * @return bool
+     */
+    public static function isLatest()
+    {
+        return static::compare(static::getLatest()) < 1;
+    }
+} 
---
+++ tests/unit/Version/VersionTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Version test case
+ *
+ * @package    Test\Utility
+ * @author     Filipe Silva <silvam.filipe@gmail.com>
+ * @copyright  2014 Filipe Silva
+ * @license    http://www.opensource.org/licenses/mit-license.php MIT License
+ * @since      Version 1.0.0
+ */
+
+namespace Version;
+
+use Codeception\Util\Stub;
+use Slick\Version\Version;
+
+/**
+ * Version test case
+ *
+ * @package    Test\Utility
+ * @author     Filipe Silva <silvam.filipe@gmail.com>
+ */
+class VersionTest extends \Codeception\TestCase\Test
+{
+
+    /**
+     * Verify version information
+     * @test
+     */
+    public function checkVersion()
+    {
+        $this->assertTrue(Version::isLatest());
+        $this->assertEquals('-1', Version::compare("0.2.2"));
+        $this->assertEquals('0', Version::compare("1.0.0-dev"));
+        $this->assertEquals('1', Version::compare("100.2.2"));
+    }
+}
