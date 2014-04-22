d98f924a4206f6d27183cd40e4e863402b1a2227
Filipe Silva <filipe.silva@sata.pt>
2014-04-22 15:16:41 +0000

    Changing the Orm/Entity serialization behavior

--- src/Slick/Orm/Entity.php
+++ src/Slick/Orm/Entity.php
@@ -16,7 +16,7 @@ use Slick\Database\RecordList;
 use Slick\Orm\Entity\Column;
 use Slick\Orm\Exception;
 use Zend\EventManager\EventManagerAwareInterface;
-use Zend\EventManager\EventManagerInterface;
+use Serializable;
 
 /**
  * Entity
@@ -27,8 +27,13 @@ use Zend\EventManager\EventManagerInterface;
  * @property string primaryKey
  */
 class Entity extends AbstractEntity
-    implements EntityInterface, EventManagerAwareInterface
+    implements EntityInterface, EventManagerAwareInterface, Serializable
 {
+
+    /**
+     * Methods for entity serialization and unserialization
+     */
+    use EntitySerialization;
 
     /**
      * @readwrite
