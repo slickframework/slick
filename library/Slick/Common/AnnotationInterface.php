151376bb793bf1224a24e87dec2cc046af0ed2c4
Filipe Silva <filipe.silva@sata.pt>
2015-04-15 11:37:12 +0000

    Adding annotation interface

---
+++ library/Slick/Common/AnnotationInterface.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * This file is part of slick/common package
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Slick\Common;
+
+/**
+ * Annotation Interface used to create custom annotation classes
+ *
+ * @package Slick\Common
+ */
+interface AnnotationInterface
+{
+    /**
+     * Returns the annotations name
+     *
+     * @return string
+     */
+    public function getName();
+
+    /**
+     * Returns the value in the tag
+     *
+     * @return mixed
+     */
+    public function getValue();
+
+    /**
+     * Returns the value of a given parameter name
+     *
+     * @param string $name
+     * @return mixed
+     */
+    public function getParameter($name);
+}
